d585c167c0ead13f96f1f22faf65d9a80f616cdb
MuhammadHassan555 <149760767+Muhammadhassanlashkarwala@users.noreply.github.com>
2025-06-14 05:53:21 +0500

    header section done

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -11,12 +11,10 @@ const Header = () => {
             src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvoSWjMnaC7vczFUvPiXanhmgK-p3LignNfg&s"
             alt="logo_img"
           />
-          <div>
+          <div className="names">
             <div className="header_college_details_name">ایم ایچ ار</div>
             <div className="header_college_details_place">پاکستان، کراچی </div>
-            <div className="header_college_details_name">
-              MHR Institute of Technology,
-            </div>
+            <div className="header_college_details_name">MHR Institute...</div>
             <div className="header_college_details_place">
               Pakistan, Karachi
             </div>
@@ -24,10 +22,40 @@ const Header = () => {
         </div>
 
         <div className="header_college_details_right">
-          <a href="" target="_blank" className="header_social_media_image"></a>
-          <a href="" target="_blank" className="header_social_media_image"></a>
-          <a href="" target="_blank" className="header_social_media_image"></a>
-          <a href="" target="_blank" className="header_social_media_image"></a>
+          <div className="header_college_social_media">
+            <a href="https://www.youtube.com/@codingzone05" target="_blank">
+              <img
+                src="https://cdn-icons-png.flaticon.com/128/3670/3670147.png"
+                className="header_social_media_image"
+                alt="youtube_logo"
+              />
+            </a>
+            <a href="https://www.facebook.com/@MuhammadHassan" target="_blank">
+              <img
+                src="https://cdn-icons-png.flaticon.com/128/733/733547.png"
+                className="header_social_media_image"
+                alt="facebook_logo"
+              />
+            </a>
+            <a href="https://x.com/" target="_blank">
+              <img
+                src="https://cdn-icons-png.flaticon.com/128/5968/5968830.png"
+                className="header_social_media_image"
+                alt="twitter_logo"
+              />
+            </a>
+            <a
+              href="https://www.instagram.com/@hassan._owais_555"
+              target="_blank"
+            >
+              <img
+                src="https://th.bing.com/th/id/OIP.0wjhvLpjGf_-r-1lqG3QAQHaHw?rs=1&pid=ImgDetMain"
+                className="header_social_media_image"
+                alt="instagram_logo"
+              />
+            </a>
+          </div>
+          <input type="text" className="header_input_tags" />
         </div>
       </div>
     </div>
