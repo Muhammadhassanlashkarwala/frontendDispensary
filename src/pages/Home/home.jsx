bc03c7c27afe6cced971a5600b6637f8bbb557b8
MuhammadHassan555 <149760767+Muhammadhassanlashkarwala@users.noreply.github.com>
2025-07-15 05:19:03 +0500

    home session updated

--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import "./home.css";
 import HomeIcon from "@mui/icons-material/Home";
-
+import GroupIcon from "@mui/icons-material/Group";
+import Diversity3Icon from "@mui/icons-material/Diversity3";
+import LocalHospitalIcon from "@mui/icons-material/LocalHospital";
+import CollectionsIcon from "@mui/icons-material/Collections";
 const Home = () => {
   return (
     <div className="home">
@@ -10,8 +13,22 @@ const Home = () => {
           <div className={`home-left-option`}>
             <HomeIcon /> About Us
           </div>
+          <div className={`home-left-option`}>
+            <GroupIcon /> Staff
+          </div>
+          <div className={`home-left-option`}>
+            <Diversity3Icon /> Facilities
+          </div>
+          <div className={`home-left-option`}>
+            <LocalHospitalIcon /> Nearby Hospitals
+          </div>
+          <div className={`home-left-option`}>
+            <CollectionsIcon /> Gallery
+          </div>
         </div>
-        <div className="home-right-page">Right Side</div>
+        <div className="home-right-page">
+          <div className="home-right-header">About Us</div>
+        </div>
       </div>
     </div>
   );
