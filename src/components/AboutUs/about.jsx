156de295617373fb4741b085c21bfb8aceb0e5a0
MuhammadHassan555 <149760767+Muhammadhassanlashkarwala@users.noreply.github.com>
2025-07-27 19:15:34 +0500

    About-us updated

--- src/components/AboutUs/about.jsx
+++ src/components/AboutUs/about.jsx
@@ -2,7 +2,23 @@ import React from "react";
 import "./about.css";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div className="about-us">
+      <p>
+        The medical needs of the campus population consisting of Students, Staff
+        members and thier families are met by the Institute Health Center. The
+        Health Center has medical officer, nursing staff members and office
+        attendant. The Institute Health Center has facilities for OPD treatment.
+        Health Center has Doctor's room, Pharmacy, nursing station, observation
+        room containing semi-fowler beds for patient care and visiting
+        specialist's room where services of Gynaecologist and Physiotherapist
+        may be availed. There is a facility of ambulance 24x7 available for
+        students, faculty and staff. The ambulance is fully equipped with an
+        oxygen cylinder, first aid kit bag, emergency medicines and other
+        necessary equipment.
+      </p>
+    </div>
+  );
 };
 
 export default About;
